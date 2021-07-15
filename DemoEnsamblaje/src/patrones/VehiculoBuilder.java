91116bc577d50b21ddb87d0ecf2e9cc1464a4724
Gabriela Ramos <gabrielaramos@MacBook-Air-de-Gabriela.local>
2021-07-15 14:54:15 -0500

    Cambios

--- DemoEnsamblaje/src/patrones/VehiculoBuilder.java
+++ DemoEnsamblaje/src/patrones/VehiculoBuilder.java
@@ -1,1 +1,18 @@
-//Clase creada
+package patrones;
+
+import sinpatron.Vehiculo;
+
+
+public abstract class VehiculoBuilder {
+    protected Vehiculo v;
+    
+    
+    public Vehiculo getVehiculo(){
+        return v;
+    }
+    
+    public abstract void ConstruirVehiculo();
+
+    
+    
+}
---
+++ DemoEnsamblaje/src/patrones/VehiculoDirector.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package patrones;
+
+
+/**
+ *
+ * @author gabrielaramos
+ */
+public class VehiculoDirector {
+    private VehiculoBuilder builder;
+    
+    public VehiculoDirector(VehiculoBuilder builder){
+        this.builder = builder;
+    }
+    
+    public void ConstruirVehiculo(){
+        
+    
+    }
+    
+}
